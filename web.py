0b14a91d4b89794808e91f856b51f755c673b625
softwaretechh <136031012+softwaretechh@users.noreply.github.com>
2023-08-11 22:32:20 +0100

    web app

--- web.py
+++ web.py
@@ -14,10 +14,16 @@ st.title("My TodoList App")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity.")
 
-for todo in todos_local:
-    st.checkbox(todo)
+for index, todo in enumerate(todos_local):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos_local.pop(index)
+        functions.write_todos(todos_local)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
 
 st.text_input(label="", placeholder="Enter a todo...",
               on_change=add_todo, key='new_todo')
 
-st.session_state
+
